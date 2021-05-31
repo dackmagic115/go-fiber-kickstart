39599515e723f846667293fd494c6945e2ce7b53
dackmagic115 <pakaphon23@gmail.com>
2021-05-31 14:06:00 +0700

    folder structure

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.15
 
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/gofiber/fiber/v2 v2.10.0 // indirect
+	github.com/gofiber/fiber/v2 v2.10.0
 	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/valyala/fasthttp v1.25.0 // indirect
 	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
-	gorm.io/driver/mysql v1.1.0 // indirect
-	gorm.io/gorm v1.21.10 // indirect
+	gorm.io/driver/mysql v1.1.0
+	gorm.io/gorm v1.21.10
 )
--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/dackmagic115/go-fiber-kickstart/database"
+	"github.com/dackmagic115/go-fiber-kickstart/routes"
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	db, err := gorm.Open(mysql.Open("user:root@tcp(127.0.0.1:3306)/db"), &gorm.Config{})
-
-	if err != nil {
-		panic("Could not connect to the database")
-	}
-
-	fmt.Println(db)
-
+	database.Connect()
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	routes.Setup(app)
 
 	app.Listen(":3000")
 }
