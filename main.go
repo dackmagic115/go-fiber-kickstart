aebbad5c31af879f7b51ac2c2ab2cd480ab5ae02
dackmagic115 <pakaphon23@gmail.com>
2021-05-31 16:36:53 +0700

    add cookie

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"github.com/dackmagic115/go-fiber-kickstart/database"
 	"github.com/dackmagic115/go-fiber-kickstart/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
