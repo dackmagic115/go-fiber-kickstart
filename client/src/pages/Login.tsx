f0de9d1515a60c28d65a3375ebe48bdc26a49c9e
dackmagic115 <pakaphon23@gmail.com>
2021-06-27 14:51:10 +0700

    login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { SyntheticEvent, useState } from "react";
+import { Redirect } from "react-router";
+
+const Login = () => {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [redirect, setRedirect] = useState(false);
+
+   const submit = async (e: SyntheticEvent) => {
+      e.preventDefault()
+
+      const { data } = await axios.post("http://localhost:3000/api/register", {
+         email,
+         password
+      }, { withCredentials: true })
+
+      setRedirect(true)
+   }
+
+   if (redirect) {
+      return <Redirect to={'/'} />
+   }
+
+   return (
+      <main className="form-signin">
+         <form onSubmit={submit}>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <input className="form-control" placeholder="Email" required
+               onChange={e => setEmail(e.target.value)}
+            />
+
+            <input className="form-control" placeholder="Password" required
+               onChange={e => setPassword(e.target.value)}
+            />
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+         </form>
+      </main>
+   )
+}
+
+
+export default Login
