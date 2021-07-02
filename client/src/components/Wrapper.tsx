383fe498b30f4ad4c7fb1da319e3a632eeeb30a8
dackmagic115 <pakaphon23@gmail.com>
2021-07-02 23:43:02 +0700

    wrapper

--- client/src/components/Wrapper.tsx
+++ client/src/components/Wrapper.tsx
@@ -4,23 +4,22 @@ import Nav from './Nav';
 
 
 class Wrapper extends Component {
-          render() {
-                    return (
-                              <>
-                                        <Nav />
-                                        <div className="container-fluid">
-                                                  <div className="row">
-                                                            <Menu />
+   render() {
+      return (
+         <>
+            <Nav />
+            <div className="container-fluid">
+               <div className="row">
+                  <Menu />
 
-                                                            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-
-                                                            </main>
-                                                  </div>
-                                        </div>
-                              </>
-                    )
-          }
+                  <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                     {this.props.children}
+                  </main>
+               </div>
+            </div>
+         </>
+      )
+   }
 }
 
 export default Wrapper
